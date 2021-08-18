e849d0699391e4f132f92106441569c9d950bd3d
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2021-08-18 23:23:39 +0100

    commit

--- resourses/js/app.js
+++ resourses/js/app.js
@@ -1,3 +1,8 @@
 console.warn("hey from app.js");
 
-const s = document.getElementById('s');
+const s = document.querySelector('nav ul .drop-down');
+const drop = document.getElementById("drop-down-icon");
+
+drop.onclick = function() {
+    s.style.display = "block";
+}
