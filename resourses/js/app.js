41b7112711447a151f895473e60bfe4489a7f74a
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2021-08-20 14:16:59 +0100

    commit

--- resourses/js/app.js
+++ resourses/js/app.js
@@ -5,3 +5,12 @@ x.onclick =  function() {
     navItems.classList.toggle('drop-down-is-showing');
 }
 
+const navBar = document.querySelector('.navigation-bar');
+
+document.body.onscroll = function() {
+    console.log(window.scrollY);
+    if(window.scrollY > 5)
+        navBar.classList.add('fixed-top');
+    else
+     navBar.classList.remove('fixed-top');
+}
