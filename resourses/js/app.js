d743c9a3077d247750b916f8d171b76b8a9537eb
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2021-08-20 21:27:25 +0100

    commit

--- resourses/js/app.js
+++ resourses/js/app.js
@@ -1,16 +1,33 @@
 const x = document.querySelector('.burger-icon') ;
 const navItems = document.querySelector('nav .navigation .nav-items');
+const upBtn = document.querySelector("#up-btn");
+const navBar = document.querySelector('.navigation-bar');
+
+
 
 x.onclick =  function() {
     navItems.classList.toggle('drop-down-is-showing');
 }
 
-const navBar = document.querySelector('.navigation-bar');
 
 document.body.onscroll = function() {
-    console.log(window.scrollY);
-    if(window.scrollY > 5)
+    
+    if(window.scrollY > 5){
+
         navBar.classList.add('fixed-top');
-    else
+        upBtn.style.display = "block";
+
+    }
+
+    else{
+
      navBar.classList.remove('fixed-top');
+     upBtn.style.display = "none";
+
+    }
+
+}
+
+upBtn.onclick =_=>{
+    window.scrollTo(0,0);
 }
