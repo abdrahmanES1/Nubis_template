8c472f272c8c70ece6ae85320b8dd1304f7babaa
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2021-08-21 22:09:09 +0100

    commit

--- resourses/js/animation.js
+++ resourses/js/animation.js
@@ -1,1 +1,19 @@
 console.warn("hey from animation.js");
+
+
+
+
+
+
+// slider of costumers
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+    slidesPerView: 3,
+      spaceBetween: 30,
+    autoplay: {
+        delay: 2000,
+    },
+
+});
